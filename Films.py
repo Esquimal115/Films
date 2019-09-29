3940d588ec22edb4c302669e5f39cbaa0aa60b02
Esquimal115 <48512639+Esquimal115@users.noreply.github.com>
2019-09-29 23:11:56 +0200

    Add files via upload

---
+++ Films.py
@@ -0,0 +1,68 @@
+Taxi Driver
+El club de la lucha
+El indomable will hunting
+American History X
+Terminator 1 / 2
+Cadena Perpetua
+Reservoir dogs
+Pulp Fiction
+Kill Bill
+Django Unchained
+El Vuelo
+Lucas
+El abogado del Diablo
+American Psycho
+El maquinista
+Master and comander
+Salvar al soldado Bryan
+Men in black 1 / 2
+Dallas Bullers Club
+Ocenans Eleven
+Los intocables de Elliot Ness
+El cabo del miedo
+Blade Runner
+Tootsie
+Rain Man
+La misión (Pendiente)
+El último Mohicano (pendiente)
+Mi pie izquierdo (Pendiente)
+Matrix
+El show de truman
+La máscara
+El jovencito Frankestein
+La vida de Brian
+Mejor imposible
+K-Pax
+American Beauty
+El resplandor
+La naranja mecánica
+Contact
+Juegos de Guerra
+Proyecto X
+A quien ama Gilbert Grape?
+Beatlejuice
+Eduardo manostijeras
+Forrest Gump
+En la cuerda floja
+Asesinato en 8mm
+Sirenas
+Jhonny Mnemonic
+Alien
+St. Vincent
+Alguien voló sobre el nido del cuco
+Crash
+Super agente K9
+Socios y sabuesos
+El nombre de la rosa
+Los inmortales
+El señor de los anillos
+Big Fish
+Hellboy
+Watchmen
+Cuando harry encontró a sally
+Cowboys de Ciudad
+Hook: El capitan garfio
+El club de los poetas muertos
+Traspotting
+Mouline Rouge
+Gattaca
